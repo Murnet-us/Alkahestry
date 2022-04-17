0063a156e2049ccd654a1d7b8efa4e1349efe49a
bangplaysgames <69700079+bangplaysgames@users.noreply.github.com>
2022-04-17 13:02:34 -0500

    update mekanism.js

---
+++ kubejs/server_scripts/customRecipes/mekanism.js
@@ -0,0 +1,34 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Project Expansion Recipes Loading.')
+
+onEvent('recipes', event => {
+	// Change recipes here
+	
+	//Steel Machine Frame
+	
+	
+	//Metallurgic Infuser
+	
+	
+	//Enriching Chamber
+	
+	
+	//Crusher
+	
+	
+	//Energized Smelter
+)
+
+onEvent('item.tags', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
